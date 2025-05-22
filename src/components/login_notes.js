48e9941a96a9115461889c9d3125e993b8cf8a6f
H4616 <103203538+H4616@users.noreply.github.com>
2025-05-22 09:31:07 +0700

    login beres

--- src/components/login_notes.js
+++ src/components/login_notes.js
@@ -26,58 +26,55 @@ const Login = () => {
 		}
 	};
 
-	return (
-		<section className="hero has-background-grey-light is-fullheight is-fullwidth">
-			<div className="hero-body">
-				<div className="container">
-					<div className="columns is-centered">
-						<div className="column is-4-desktop">
-							<form onSubmit={Login} className="box">
+    return (
+        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-centered">
+                        <div className="column is-4-desktop">
+                            <form onSubmit={Login} className="box">
                                 <p className="has-text-centered mt-5">{msg}</p>
-								<div className="field mt-5">
-									<label className="label">Email</label>
-									<div className="control">
-										<input
-											className="input"
-											type="email"
-											placeholder="Enter your email"
+                                <div className="field mt-5">
+                                    <label className="label">Email</label>
+                                    <div className="control">
+                                        <input
+                                            className="input"
+                                            type="email"
+                                            placeholder="Enter your email"
                                             value={email}
                                             onChange={(e) => setEmail(e.target.value)}
-										/>
-									</div>
-								</div>
-								<div className="field mt-5">
-									<label className="label">Password</label>
-									<div className="control">
-										<input
-											className="input"
-											type="password"
-											placeholder="***********"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Password</label>
+                                    <div className="control">
+                                        <input
+                                            className="input"
+                                            type="password"
+                                            placeholder="***********"
                                             value={password}
                                             onChange={(e) => setPassword(e.target.value)}
-										/>
-									</div>
-								</div>
-								<div className="field mt-5">
-									
-										<button className="button is-success is-fullwidth">
-                                            onClick={Login}
-											Login
-										</button>
-									
-								</div>
+                                        />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <button className="button is-success is-fullwidth" type="submit">
+                                        Login
+                                    </button>
+                                </div>
                                 <div>
                                     <p className="has-text-centered mt-5">
                                         Don't have an account? <a href="/register">Register</a>
                                     </p>
                                 </div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Login;
