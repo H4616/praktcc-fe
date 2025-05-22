fdbbb2bd42c92ad6135f9f1bc77b31d5353110f9
H4616 <103203538+H4616@users.noreply.github.com>
2025-05-22 11:29:40 +0700

    cape bang

--- src/components/Add_note.js
+++ src/components/Add_note.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { BASE_URL } from "../utils.js";
 
 const AddNote = () => {	
